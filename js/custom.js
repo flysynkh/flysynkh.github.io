ceeac75680c9c0f55c4a143b823b433417d9d9ed
flysynkh <45093313+flysynkh@users.noreply.github.com>
2019-02-06 18:37:05 +0530

    flyweight new

--- js/custom.js
+++ js/custom.js
@@ -12,6 +12,7 @@
 6. Init CTA Slider
 7. Init Testimonials Slider
 8. Init Search Form
+ 9.
 
 
 ******************************/
@@ -404,4 +405,24 @@ $(document).ready(function()
 		}
 	}
     $('#preloader').fadeOut();
+
+    /* Back Top Link active
+  ========================================================*/
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.back-to-top').fadeIn(400);
+        } else {
+            $('.back-to-top').fadeOut(400);
+        }
+    });
+
+    $('.back-to-top').on('click',function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
 });
